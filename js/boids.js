e12b32deea5d92977416697a3b69c4306bfdb871
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-08-31 20:32:21 +0200

    Beginning' git push origin master

--- js/boids.js
+++ js/boids.js
@@ -2,7 +2,17 @@ class Boids {
     constructor(x, y) {
       this.x = x;
       this.y = y;
+      this.dx;
+      this.dy;
     }
-    move(){}
+    getDistance(other){
+        dx = this.x - other.x;
+        dy = this.y - other.y;
+    }
+    getTotalAcceleration(){
 
+    }
+    modifyAcceleration(a){
+w
+    }
   }
