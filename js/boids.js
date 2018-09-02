e525f031ceebc3369c4908685be4f87c1d7899e8
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-09-02 21:26:55 +0200

    Some style

--- js/boids.js
+++ js/boids.js
@@ -1,101 +1,102 @@
-function Boids(x, y) {
-  //Each BOID is represented by a position
-  this.x = x || 0;
-  this.y = y || 0;
-  //delta distance
-  this.dx = 0;
-  this.dy = 0;
-  this.maxDist;
-  //speed
-  this.speed = (1, 1);
-  this.maxSpeed = (2, 2);
-  this.acceleration = (0, 0);
-  //forces equilibrium
-  this.sepWeight = 0.5;
-  this.cohWeight = 0.5;
-  this.aliWeight = 0.5;
+class Boid {
+  constructor(x, y) {
+    this.run = run;
+    //Each BOID is represented by a position
+    this.x = x || 0;
+    this.y = y || 0;
+    //delta distance
+    this.dx = 0;
+    this.dy = 0;
+    this.maxDist;
+    //speed
+    this.speed = (1, 1);
+    this.maxSpeed = (2, 2);
+    this.acceleration = (0, 0);
+    //forces equilibrium
+    this.sepWeight = 0.5;
+    this.cohWeight = 0.5;
+    this.aliWeight = 0.5;
+  }
+  //Return the position coordinates of the void
+  /* pos() {
+    return this.x, this.y;
+  }
+  set(nX, nY) {
+    this.x = nX;
+    this.y = nY;
+    return this;
+  } */
+  //Vector operations
+ /*  length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+  add(v) {
+    this.x += v.x;
+    this.y += v.y;
+    return this;
+  }
+  sub(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+    return this;
+  }
+  normalize(a) {
+    var l = this.length();
+    this.x /= l;
+    this.y /= l;
+    if (value != null)
+      this.multiplyScalar(value);
+    return this;
+  }
+  multiplyScalar(a) {
+    this.x *= a;
+    this.y *= a;
+    return this;
+  }
+  negate() {
+    this.x *= -1;
+    this.y *= -1;
+    return this;
+  } */
+  //Get data
+ /*  initialize() {
+    this.speed.x += (Math.random() - 0.5) * 2;
+    this.speed.y += (Math.random() - 0.5) * 2;
+  }
+  getDistance(other) {
+    this.dx = this.x - other.x;
+    this.dy = this.y - other.y;
+  }
+  getOther() {
+    return other;
+  } */
+  //Forces
+/*   separation() { }
+  cohesion() { }
+  alignment() { }
+  getTotalAcceleration() { }
+  modifyAcceleration(a) { } */
+  //Animation
+  /* Boid.prototype.move = function() {}; */
+  draw() {
+    var size = 2;
+    this.run.ctx.fillStyle = "black";
+    this.run.ctx.beginPath();
+    this.run.ctx.arc(this.x, this.y, size, 0, Math.PI * 2);
+    this.run.ctx.fill();
+  }
 }
 
-//Return the position coordinates of the void
-Boids.prototype.pos = function() {
-  return this.x, this.y;
-};
-Boids.prototype.set = function(nX, nY) {
-  this.x = nX;
-  this.y = nY;
-  return this;
-};
-
-//Vector operations
-Boids.prototype.length = function() {
-  return Math.sqrt(this.x * this.x + this.y * this.y);
-};
-Boids.prototype.add = function(v) {
-  this.x += v.x;
-  this.y += v.y;
-  return this;
-};
-Boids.prototype.sub = function(v) {
-  this.x -= v.x;
-  this.y -= v.y;
-  return this;
-};
-Boids.prototype.normalize = function(a) {
-  var l = this.length();
-  this.x /= l;
-  this.y /= l;
-  if (value != null) this.multiplyScalar(value);
-  return this;
-};
-Boids.prototype.multiplyScalar = function(a) {
-  this.x *= a;
-  this.y *= a;
-  return this;
-};
-Boids.prototype.negate = function() {
-  this.x *= -1;
-  this.y *= -1;
-  return this;
-};
-
-//Get data
-Boids.prototype.initialize = function() {
-  this.speed.x += (Math.random() - 0.5) * 2;
-  this.speed.y += (Math.random() - 0.5) * 2;
-};
-Boids.prototype.getDistance = function(other) {
-  this.dx = this.x - other.x;
-  this.dy = this.y - other.y;
-};
-Boids.prototype.getOther = function() {
-  return other;
-};
-
-//Forces
-Boids.prototype.separation = function() {};
-Boids.prototype.cohesion = function() {};
-Boids.prototype.alignment = function() {};
-Boids.prototype.getTotalAcceleration = function() {};
-Boids.prototype.modifyAcceleration = function(a) {};
-
-//Animation
-Boids.prototype.move = function() {};
-Boids.prototype.draw = function() {
-  var size = 2;
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, size, 0, Math.PI * 2);
-  ctx.fill();
-};
 //End of Class
 
 //Other Math
-function getAngle(a, b) {
+/* function getAngle(a, b) {
   var dx = b.x - a.x;
   var dy = b.y - a.y;
   return Math.atan2(dy, dx);
-}
-function getDistance(a, b) {
+} */
+/* function getDistance(a, b) {
   var dx = a.x - b.x;
   var dy = a.y - b.y;
   return Math.sqrt(dx * dx + dy * dy);
-}
+} */
--- js/run.js
+++ js/run.js
@@ -1,114 +1,95 @@
 //Define the canvas
-function Run(canvadId) {
-  this.canvas = document.getElementById(canvadId);
-  this.ctx = this.canvas.getContext("2d");
-  this.fps = 60;
-
-  this.reset();
-}
-
-//Start gets the animations going
-Run.prototype.start = function() {
-  this.interval = setInterval(
-    function() {
+class Run {
+  constructor(canvasId) {
+    this.canvas = document.getElementById(canvasId);
+    this.ctx = this.canvas.getContext("2d");
+    this.fps = 60;
+    this.boids = [];
+    this.reset();
+  }
+  //Start gets the animations going
+  start() {
+    this.interval = setInterval(function () {
       this.clear();
-
       this.framesCounter++;
-
       //FrameCounter max 1000
       if (this.framesCounter > 1000) {
         this.framesCounter = 0;
       }
-
       this.score += 0.01;
-
-      this.moveAll();
-      this.draw();
-
-    }.bind(this),
-    1000 / this.fps
-  );
-};
-
-Run.prototype.stop = function() {
-  clearInterval(this.interval);
-};
-
-Run.prototype.gameOver = function() {
-  this.stop();
-
-  if (confirm("GAME OVER. Run again?")) {
-    this.reset();
-    this.start();
+      //Move & Draw
+      //this.moveAll();
+      this.drawAll();
+    }.bind(this), 1000 / this.fps);
   }
-};
-
-Run.prototype.clear = function() {
-  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-};
-
-////Actualize!!!
-
-Run.prototype.reset = function() {
-  var boid,x,y;
-  this.boids = [];
-  for (var i = 0; i < 50; i++) {
-    //generate boids in random position
-    x = Math.random() * this.canvas.width;
-    y = Math.random() * this.canvas.height
-    boid = new Boid (x, y);
-    boid.acceleration = (0, 0);
-    boid.speed = (Math.random() - 0.5, Math.random() - 0.5);
-    boid.maxSpeed = (2, 2);
-  /*   boid.acceleration = new Boid(0, 0);
-    boid.speed = new Boid (Math.random() - 0.5, Math.random() - 0.5);
-    boid.maxSpeed = new Boid (2, 2); */
-
-
-    boids.push(boid);
-  };
-  boids.forEach(function(boid) {
-    boid.draw();
-    console.log(boy.x,boid.y);
-  });
-
-  this.framesCounter = 0;
-  this.score = 0;
-};
-
-/* Run.prototype.draw = function() {
-  this.background.draw();
-  this.player.draw();
-  this.obstacles.forEach(function(obstacle) {
-    obstacle.draw();
-  });
-  this.drawScore();
-}; */
-
-/* Run.prototype.isCollision = function() {
-  // colisiones genéricas
-  // (p.x + p.w > o.x && o.x + o.w > p.x && p.y + p.h > o.y && o.y + o.h > p.y )
-  return this.obstacles.some(
-    function(obstacle) {
-      return (
-        this.player.x + this.player.w >= obstacle.x &&
+ /*  stop() {
+    clearInterval(this.interval);
+  } */
+/*   gameOver() {
+    this.stop();
+    if (confirm("GAME OVER. Run again?")) {
+      this.reset();
+      this.start();
+    }
+  } */
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "#FA959F";
+  }
+  ////Actualize!!!
+  reset() {
+    for (var i = 0; i < 50; i++) {
+      //generate boids in random position
+      var x = Math.random() * this.canvas.width;
+      var y = Math.random() * this.canvas.height;
+      this.boid = new Boid(x, y);
+      this.boid.acceleration = (0, 0);
+      this.boid.speed = (Math.random() - 0.5, Math.random() - 0.5);
+      /*   this.boid.maxSpeed = (2, 2);
+      this.boid.acceleration = new Boid(0, 0);
+      this.boid.speed = new Boid (Math.random() - 0.5, Math.random() - 0.5);
+      this.boid.maxSpeed = new Boid (2, 2); */
+      this.boids.push(this.boid);
+    }
+    this.framesCounter = 0;
+    this.score = 0;
+  }
+  drawAll() {
+    this.boids.forEach(function (boid) {
+      boid.draw();
+      console.log(boy.x, boid.y);
+    });
+    //this.drawScore();
+  }
+/*   isCollision() {
+    // colisiones genéricas
+    // (p.x + p.w > o.x && o.x + o.w > p.x && p.y + p.h > o.y && o.y + o.h > p.y )
+    return this.obstacles.some(function (obstacle) {
+      return (this.player.x + this.player.w >= obstacle.x &&
         this.player.x < obstacle.x + obstacle.w &&
-        this.player.y + (this.player.h - 20) >= obstacle.y
-      );
-    }.bind(this)
-  );
-}; */
+        this.player.y + (this.player.h - 20) >= obstacle.y);
+    }.bind(this));
+  } */
+ /*  moveAll() {
+    this.background.move();
+    this.player.move();
+    this.obstacles.forEach(function (obstacle) {
+      obstacle.move();
+    });
+  } */
+/*   drawScore() {
+    this.ctx.font = "30px sans-serif";
+    this.ctx.fillStyle = "black";
+    this.ctx.fillText(Math.floor(this.score), 50, 50);
+  } */
+}
 
-/* Run.prototype.moveAll = function() {
-  this.background.move();
-  this.player.move();
-  this.obstacles.forEach(function(obstacle) {
-    obstacle.move();
-  });
-}; */
 
-Run.prototype.drawScore = function() {
-  this.ctx.font = "30px sans-serif";
-  this.ctx.fillStyle = "black";
-  this.ctx.fillText(Math.floor(this.score), 50, 50);
-};
+
+
+
+
+
+
+
+
