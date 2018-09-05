509d8f75746c7f53faac0f567833d62e19557748
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-09-05 11:05:41 +0200

    Some bugs solved & fixed structure

--- js/vector.js
+++ js/vector.js
@@ -2,41 +2,41 @@ function Vector(x, y) {
   this.x = x;
   this.y = y;
 }
-Run.prototype.set = function(nX, nY) {
+Vector.prototype.set = function(nX, nY) {
   this.x = nX;
   this.y = nY;
   return this;
 };
-Run.prototype.length = function() {
+Vector.prototype.length = function() {
   if (this.x != 0 || this.y != 0) {
     return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
   } else {
     return 0;
   }
 };
-Run.prototype.add = function(v) {
+Vector.prototype.add = function(v) {
   this.x += v.x;
   this.y += v.y;
   return this;
 };
-Run.prototype.sub = function(v) {
+Vector.prototype.sub = function(v) {
   this.x -= v.x;
   this.y -= v.y;
   return this;
 };
-Run.prototype.normalize = function(a) {
+Vector.prototype.normalize = function(a) {
   var l = this.length();
   this.x == 0 ? (this.x = 0) : (this.x /= l);
   this.y == 0 ? (this.y = 0) : (this.y /= l);
   this.multiplyScalar(a);
   return this;
 };
-Run.prototype.multiplyScalar = function(a) {
+Vector.prototype.multiplyScalar = function(a) {
   this.x *= a;
   this.y *= a;
   return this;
 };
-Run.prototype.negate = function() {
+Vector.prototype.negate = function() {
   this.x *= -1;
   this.y *= -1;
   return this;
